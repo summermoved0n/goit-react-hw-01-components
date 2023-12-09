7df43d5e1e848eccdee3544c6e14c68eba374f63
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-12-09 11:58:15 -0500

    done react hw1 and 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { Fragment } from "react";
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import user from '../data/user'
+import data from '../data/data'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Fragment>
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </Fragment>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,18 @@
+import css from './statistics.module.css'
+
+export const Statistics = ({title, stats}) => {
+  return (
+    <section className="statistics">
+      {title ? <h2 className="title">{title}</h2> : null}
+
+  <ul className="stat-list">
+    {stats.map(({id, label, percentage}) => (
+      <li className="item" key={id}>
+        <span className="label">{label}</span>
+        <span className="percentage">{ percentage }%</span>
+    </li>
+    ))}
+  </ul>
+</section>
+  )
+}
