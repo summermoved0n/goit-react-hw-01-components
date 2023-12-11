105160fa6a0fdf536ded67dfc69902ec2c41072e
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-12-11 13:37:51 -0500

    add helpers.js

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,23 +1,23 @@
-import css from './statistics.module.css'
+import css from './statistics.module.css';
+import { generateRandomColor } from '../helpers/helpers';
 
-const generateRandomColor = () => {
-  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  return randomColor;
-};
-
-export const Statistics = ({title, stats}) => {
+export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title ? <h2 className={css.title}>{title}</h2> : null}
+      {title && <h2 className={css.title}>{title}</h2>}
 
-  <ul className={css.stat_list}>
-    {stats.map(({id, label, percentage}) => (
-      <li className={css.item} key={id} style={{ backgroundColor: generateRandomColor() }}>
-        <span className={css.label}>{label}</span>
-        <span className={css.percentage}>{ percentage }%</span>
-    </li>
-    ))}
-  </ul>
-</section>
-  )
-}
+      <ul className={css.stat_list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            key={id}
+            style={{ backgroundColor: generateRandomColor() }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
